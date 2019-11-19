ad7baa1d93546e3b0bd6049f3272e27eb59287f5
Plank1001 <50321834+Plank1001@users.noreply.github.com>
2019-11-19 12:50:32 +0100

    Update views.py

--- aplikacja do nauki/djangogirls/mysite/blog/views.py
+++ aplikacja do nauki/djangogirls/mysite/blog/views.py
@@ -3,3 +3,5 @@ from django.utils import timezone
 from .models import Post
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+def inna(request):
+    return render(request, 'blog/inna.html')
